f6a1bf0a47eebbd3c736ab1f59ca2a44184574e0
Ian Marshall <imarshall14@gmail.com>
2021-03-29 20:40:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, ed, dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, white, black, pink, grey] = farmAnimals.split(' ');
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+const [r, o, y, g, b, i, v] = colors
+const [,,,,,indg,] = colors
+
+// const []
 const muppet = {
   muppetName: 'Miss Piggy',
   color: 'pink',
@@ -24,6 +33,12 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {muppetName, color, song, job, partner} = muppet
+
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+// const [,,song4] = nestedMuppet.album.theMuppetMovie.song4
+const {nestedJob, nestedPartner} = nestedMuppet
 
 // Strings
 
